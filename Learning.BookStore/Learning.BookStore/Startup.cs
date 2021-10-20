c81e5af5abea7112e97c859e41d1bcc7751e0c88
Santanu <santanu.roy334@gmail.com>
2021-10-20 18:21:57 +0530

    Add MVC to application

--- Learning.BookStore/Learning.BookStore/Startup.cs
+++ Learning.BookStore/Learning.BookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace Learning.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +27,29 @@ namespace Learning.BookStore
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from my first middleware ");
+
+            //    await next();
+
+            //    await context.Response.WriteAsync("Hello from my first middleware 2 ");
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from my second middleware ");
+            //});
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
